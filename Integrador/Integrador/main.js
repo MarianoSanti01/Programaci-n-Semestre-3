f817da38413f4417d2cd5aac3510e82ba6dd7a95
Mariano <marianosanti2014@gmail.com>
2024-09-18 22:07:44 -0300

    18/9

--- Integrador/Integrador/main.js
+++ Integrador/Integrador/main.js
@@ -1,3 +1,3 @@
 import { renderCategories } from "./src/services/categories";
-
+import './style.css'
 renderCategories();
--- Integrador/Integrador/src/services/categories.js
+++ Integrador/Integrador/src/services/categories.js
@@ -1,8 +1,10 @@
 //Render de la vista categorias//
 
-export const renderCategories = ()=> {
-    const ulList = document.getElementById(listFilter);
-    ulList.innerHTML =`
+export const renderCategories = () => {
+    //Tomamos elementos de la lista
+    const ulList = document.getElementById("listFilter");
+    //creamos elementos dentro de la lista
+    ulList.innerHTML = `
     <li id="Todo">Todos Los productos</li>
     <li id="Hamburguesas">Hamburguesas</li>
     <li id="Papas">Papas</li>
@@ -10,4 +12,25 @@ export const renderCategories = ()=> {
     <li id="mayorPrecio">Mayor Precio</li>
     <li id="menorPrecio">Menor Precio</li>
     `;
+//añadimos dinamicamente el evento click
+
+    const liElements = ulList.querySelectorAll("li")
+    liElements.forEach((liElement) => {
+        liElement.addEventListener('click', () => {
+            handleClick(liElement);
+        });
+    });
+
+//verificamos y manejamos el estilo del elemnto activo
+    const handleClick = (elemento) => {
+        liElements.forEach((el) => {
+            if (el.classList.contains("liActive")) {
+            el.classList.remove("liActive");
+        } else {
+            if (elemento === el){
+                el.classList.add("liActive");
+        }
+    }
+});
+    };
 };
